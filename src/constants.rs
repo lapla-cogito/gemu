e61e2c67449c536fbff68db844c09a2ff360ef91
lapla-cogito <kakikei20021108@gmail.com>
2023-11-26 14:43:55 +0900

    Enable PPU rendering

--- src/constants.rs
+++ src/constants.rs
@@ -5,9 +5,13 @@ pub const BG_WINDOW_TILE_DATA_SELECT: u8 = 1 << 4;
 pub const BG_TILE_MAP_SELECT: u8 = 1 << 3;
 pub const OBJ_SIZE: u8 = 1 << 2;
 pub const OBJ_ENABLE: u8 = 1 << 1;
-pub const BG_ENABLE: u8 = 1 << 0;
+pub const BG_DISPLAY_ENABLE: u8 = 1 << 0;
 pub const LYC_EQ_LY_INT: u8 = 1 << 6;
 pub const OAM_SCAN_INT: u8 = 1 << 5;
 pub const VBLANK_INT: u8 = 1 << 4;
 pub const HBLANK_INT: u8 = 1 << 3;
 pub const LYC_EQ_LY: u8 = 1 << 2;
+
+pub const LCD_WIDTH: usize = 160;
+pub const LCD_HEIGHT: usize = 144;
+pub const LCD_PIXELS: usize = LCD_WIDTH * LCD_HEIGHT;
--- src/ppu.rs
+++ src/ppu.rs
@@ -23,6 +23,8 @@ pub struct Ppu {
     wx: u8,
     vram: Box<[u8; 0x2000]>,
     oam: Box<[u8; 0xa0]>,
+    cycles: u8,
+    buffer: Box<[u8; LCD_PIXELS * 4]>,
 }
 
 impl Ppu {
@@ -42,6 +44,8 @@ impl Ppu {
             wx: 0,
             vram: Box::new([0; 0x2000]),
             oam: Box::new([0; 0xa0]),
+            cycles: 20,
+            buffer: Box::new([0; LCD_PIXELS * 4]),
         }
     }
 
@@ -102,5 +106,104 @@ impl Ppu {
             0xFF4B => self.wx = data,
             _ => unreachable!(),
         }
+    }
+
+    fn get_pixel_from_tile(&self, tile_ind: usize, y: u8, x: u8) -> u8 {
+        let r = (y * 2) as usize;
+        let c = (7 - x) as usize;
+        let tile_addr = tile_ind << 4;
+        let row = self.vram[(tile_addr | r) & 0x1fff];
+        let high = self.vram[(tile_addr | (r + 1)) & 0x1fff];
+        (((high >> c) & 1) << 1) | ((row >> c) & 1)
+    }
+
+    fn get_tile_idx_from_tile_map(&self, tile_map: bool, y: u8, x: u8) -> usize {
+        let start_addr: usize = 0x1800 | ((tile_map as usize) << 10);
+        let ret = self.vram[start_addr | (((y as usize) << 5) + x as usize) & 0x3ff];
+        if self.lcdc & BG_WINDOW_TILE_DATA_SELECT > 0 {
+            ret as usize
+        } else {
+            ((ret as i8 as i16) + 0x100) as usize
+        }
+    }
+
+    fn check_lyc_eq_ly(&mut self) {
+        if self.ly == self.lyc {
+            self.stat |= LYC_EQ_LY;
+        } else {
+            self.stat &= !LYC_EQ_LY;
+        }
+    }
+
+    fn render(&mut self) {
+        if self.lcdc & BG_DISPLAY_ENABLE == 0 {
+            return;
+        }
+
+        let y = self.ly.wrapping_add(self.scy);
+        for i in 0..LCD_WIDTH {
+            let x = (i as u8).wrapping_add(self.scx);
+            let tile_ind =
+                self.get_tile_idx_from_tile_map(self.lcdc & BG_TILE_MAP_SELECT > 0, y >> 3, x >> 3);
+
+            let pixel = self.get_pixel_from_tile(tile_ind as usize, y & 7, x & 7);
+
+            self.buffer[i + (self.ly as usize) * LCD_WIDTH] =
+                match (self.bgp >> (pixel << 1)) & 0b11 {
+                    0b00 => 0xff,
+                    0b01 => 0xaa,
+                    0b10 => 0x55,
+                    _ => 0x00,
+                };
+        }
+    }
+
+    fn emu(&mut self) -> bool {
+        // Check if PPU is enabled
+        if self.lcdc & PPU_ENABLE == 0 {
+            return false;
+        }
+
+        self.cycles -= 1;
+        if self.cycles > 0 {
+            return false;
+        }
+
+        let mut ret = false; // Is VSYNC
+        match self.mode {
+            Mode::OAMScan => {
+                self.mode = Mode::Drawing;
+                self.cycles = 43;
+            }
+            Mode::Drawing => {
+                self.render();
+                self.mode = Mode::HBlank;
+                self.cycles = 51;
+            }
+            Mode::HBlank => {
+                self.ly = self.ly.wrapping_add(1);
+                if self.ly < 114 {
+                    self.mode = Mode::OAMScan;
+                    self.cycles = 20;
+                } else {
+                    self.mode = Mode::VBlank;
+                    self.cycles = 114;
+                }
+                self.check_lyc_eq_ly();
+            }
+            Mode::VBlank => {
+                self.ly = self.ly.wrapping_add(1);
+                if self.ly > 153 {
+                    ret = true;
+                    self.ly = 0;
+                    self.mode = Mode::OAMScan;
+                    self.cycles = 20;
+                } else {
+                    self.cycles = 114;
+                }
+                self.check_lyc_eq_ly();
+            }
+        }
+        ret
     }
 }
